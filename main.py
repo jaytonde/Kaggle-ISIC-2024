3637ce29eb509ab2d835da49a08d421b9be449ff
Jaydev Tonde <47071053+jaytonde@users.noreply.github.com>
2024-07-13 12:57:19 +0530

    iurtyer

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ class ISICDataset:
         image_data    = self.image_file[image_id][()]
         pil_image     = Image.open(io.BytesIO(image_data))
         tensor_image  = self.transform(pil_image)
-        tensor_target = torch.tensor(self.df.iloc[idx]['target'])
+        tensor_target = torch.tensor(self.df.iloc[idx]['target'], dtype = torch.long)
         
         return {'image':tensor_image, 'label':tensor_target}
 

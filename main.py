2c61ca92b4fc2c4a4ae928a1945880239f8aef88
Jaydev Tonde <47071053+jaytonde@users.noreply.github.com>
2024-07-15 20:54:13 +0530

    risuujfd

--- main.py
+++ main.py
@@ -195,7 +195,9 @@ def get_transform(mode):
 
     elif mode == "test":
         transform = transforms.Compose([
+            transforms.Resize((224, 224)),
             transforms.ToTensor(),  # Convert PIL Image to tensor
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
         return transform
     else:
